2b583511f7d6a43acf5e841a375d192a13e39dcc
Aby Kal <abyabrkal@gmail.com>
2020-12-14 21:23:32 +0400

    Add more ui updates

--- hangman.py
+++ hangman.py
@@ -2,75 +2,21 @@
 # HANGMAN
 
 import random
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
+from hangman_data import word_list, stages, logo
 
 word_list = ['ardvark', 'baboon', 'camel']
 chosen_word = random.choice(word_list)
 lives = 6
 
+print(logo)
 display = ['_' for _ in chosen_word]
-print(display)
 
 end_of_game = False
 while not end_of_game:
   guess = input("Enter a letter: ").lower()
+
+  if guess in display:
+    print(f"You have already guessed this letter {guess}.")
 
   for position in range(len(chosen_word)):
     letter = chosen_word[position]
@@ -80,15 +26,16 @@ while not end_of_game:
       display[position] = letter
 
   if guess not in chosen_word:
+    print(f"You guessed a letter not in the word. You LOSE 1 LIFE.")
     lives -= 1
     if lives == 0:
       end_of_game = True
-      print(f"Correct word: {chosen_word}. You lose.")
+      print(f"Correct word: < {chosen_word} >. You LOSE.")
 
   print(f"{' '.join(display)}")
 
   if '_' not in display:
     end_of_game = True
-    print('You win.')
+    print('You WIN.')
 
   print(stages[lives])
