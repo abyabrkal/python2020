f93a2a9213ee3e0a5efa8195a8d27f7c9f3deef0
Aby Kal <abyabrkal@gmail.com>
2021-01-22 19:15:56 +0400

    New text entry prompt

--- Guess-US-States/main.py
+++ Guess-US-States/main.py
@@ -1,7 +1,20 @@
 import turtle
 
 screen = turtle.Screen()
-screen.title("Guess US States")
+screen.title("Guess Game - US States")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state = screen.textinput(title="Guess Game - US States", prompt="Enter the state's name?")
+
+#def get_mouse_click_coords(x, y):
+#  print(x, y)
+
+## prints out the x, y coordinates of a mouse click
+#turtle.onscreenclick(get_mouse_click_coords)
+#turtle.mainloop()
+## This gives the data to consle and waits at console.
 
 
 screen.exitonclick()
