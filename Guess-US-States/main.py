9970930b41c0ee5c216b9016d2a48d5ccd23f5f5
Aby Kal <abyabrkal@gmail.com>
2021-01-23 11:52:24 +0400

    States guess game completed

--- Guess-US-States/main.py
+++ Guess-US-States/main.py
@@ -13,22 +13,32 @@ all_states = data.state.to_list()
 guessed_state = []
 
 while len(guessed_state) < 50:
-  answer_state = screen.textinput(title="Guess Game - US States", prompt="Enter the state's name?")
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States guessed",
+                                    prompt="Enter the state's name?").title()
 
-  if answer_state in all_states:
-    guessed_state.append(answer_state)
-      t = turtle.Turtle()
-      t.hideturtle()
-      t.penup()
-      t.color('blue')
-      state_data = data[data.state == answer_state]
-      t.goto(int(state_data.x), int(state_data.y))
-      t.write(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+          if state not in guessed_state:
+            missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.color('blue')
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 
 
 
-screen.exitonclick()
+
     
 # if answer_state 
 #def get_mouse_click_coords(x, y):
