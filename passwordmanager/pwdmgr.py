7ebfb0bef00561ba20e9b49acde9452bfa8aae5b
Aby Kal <abyabrkal@gmail.com>
2021-02-22 14:53:27 +0400

    Password manager is ready with passowrd generator

--- passwordmanager/pwdmgr.py
+++ passwordmanager/pwdmgr.py
@@ -1,7 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+  nr_letters = random.randint(8, 10)
+  nr_symbols = random.randint(2, 4)
+  nr_numbers = random.randint(2, 4)
+
+  password_letters = [random.choice(letters) for _ in range(nr_letters)]
+  password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+  password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+  password_list = password_letters + password_symbols + password_numbers 
+  random.shuffle(password_list)
+
+  password = ''.join(password_list)
+  pass_input.delete(0, END)
+  pass_input.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -58,7 +78,7 @@ pass_label.grid(column=0, row=3)
 pass_input = Entry(width=21)
 pass_input.grid(column=1, row=3)
 
-pass_button = Button(text="Generate Password")
+pass_button = Button(text="Generate Password", command=generate_password)
 pass_button.grid(column=2, row=3)
 
 # *************** ROW4  **************
