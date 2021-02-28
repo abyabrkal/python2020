4d535b9f75e121d7ec84bac18bcafc0d2a059261
Aby Kal <abyabrkal@gmail.com>
2021-02-28 11:38:14 +0400

    Corr

--- api-rest/rain-alert.py
+++ api-rest/rain-alert.py
@@ -5,10 +5,12 @@ import os
 from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 
+
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = os.environ.get("OWM_API_KEY")
 account_sid = "YOUR ACCOUNT SID"
 auth_token = os.environ.get("AUTH_TOKEN")
+
 
 weather_params = {
     "lat": "YOUR LATITUDE",
