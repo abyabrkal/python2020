fd0a6821901c64595a251087a85bb02e998f52a6
Aby Kal <abyabrkal@gmail.com>
2021-03-02 15:22:50 +0400

    Updating Sheety request

--- api-rest/WorkoutTrackingSheety/workouttracksheety.py
+++ api-rest/WorkoutTrackingSheety/workouttracksheety.py
@@ -29,3 +29,25 @@ response = requests.post(url=nutr_endpoint, json=nutr_data, headers=headers)
 result = response.json()
 print(result)
 
+
+workout_date = datetime.now().strftime("%d/%m/%Y")
+workout_time = datetime.now().strftime("%X")
+
+sheety_endpoint = 'https://api.sheety.co/0b5ce03c8fff1b3df4cc1a751a285277/workoutsTracking/workouts'
+
+
+for exercise in result['exercises']:
+    sheety_data = {
+        'workout': {
+            "date": workout_date,
+            "time": workout_time,
+            "exercise": exercise['name'].title(),
+            "duration": exercise['duration_min'],
+            "calories": exercise['nf_calories'],
+        }
+    }
+
+
+sheety_response = requests.post(url=sheety_endpoint, json=sheety_data)
+# sheety_response = requests.get(sheety_endpoint)
+print(sheety_response.text)
