e0da2a919e9e3007db6d7578009fa876df90f21e
Aby Kal <abyabrkal@gmail.com>
2021-03-07 13:36:53 +0400

    Making the html soup

--- BeautifulSoup/bs-web.py
+++ BeautifulSoup/bs-web.py
@@ -1,3 +1,13 @@
 from bs4 import BeautifulSoup
+# import lxml
 
-soup = BeautifulSoup(website, 'html.parser')
+
+with open('website.html') as file:
+  contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+print(soup.title) # returns title tag
+print(soup.title.string)  # returns the string in title tag
+
+print(soup)     # entire html soup
+print(soup.prettify())    # prettify the html
