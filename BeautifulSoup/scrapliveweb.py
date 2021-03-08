5e85bb29cadf05c4c2f0b4acfe2740584f2c81ec
Aby Kal <abyabrkal@gmail.com>
2021-03-08 16:22:46 +0400

    Scrap HackerNews

--- BeautifulSoup/scrapliveweb.py
+++ BeautifulSoup/scrapliveweb.py
@@ -5,10 +5,16 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
 
 yc_soup = BeautifulSoup(yc_web_page, 'html.parser')
-article_tag = yc_soup.find(name='a', class_='storylink')
-article_text = article_tag.getText()
-article_link = article_tag.get("href")
-article_upvote = yc_soup.find(name='span', class_='score').getText()
+articles = yc_soup.find_all(name='a', class_='storylink')
+articles_texts = []
+articles_links = []
 
-print(article_upvote)
+for article_tag in articles:
+    text = article_tag.getText()
+    articles_texts.append(text)
+    link = article_tag.get("href")
+    articles_links.append(link)
 
+article_upvotes = [score.getText().split(' ')[0] for score in yc_soup.find_all(name='span', class_='score')]
+
+print(article_upvotes)
